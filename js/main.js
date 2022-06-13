3df10a98422c5954febeb5c3fb1125ab1777e1c9
Andrez Ramirez <andrezzz2312@gmail.com>
2022-06-13 10:02:24 -0500

    threejs

--- js/main.js
+++ js/main.js
@@ -2,31 +2,30 @@ import * as THREE from '../node_modules/three/build/three.module.js'
 // import {OrbitControls} from '../node_modules/three/examples/js/controls/OrbitControls.js'
 
 // const controls = new OrbitControls(camera, renderer.domElement)
-const scene = new THREE.Scene()
-// const canvas = document.querySelector('.canvas')
-const canvas = document.querySelector('.three')
-const camera = new THREE.PerspectiveCamera(
-  75,
-  canvas.offsetWidth / canvas.offsetHeight,
-  0.1,
-  1000
-)
+let scene, camera, renderer, cube, canvas
+function init() {
+  scene = new THREE.Scene()
+  canvas = document.querySelector('.three')
+  camera = new THREE.PerspectiveCamera(
+    75,
+    canvas.offsetWidth / canvas.offsetHeight,
+    0.1,
+    1000
+  )
 
-const renderer = new THREE.WebGLRenderer()
+  renderer = new THREE.WebGLRenderer({alpha: true, antialias: true})
 
-renderer.setSize(canvas.offsetWidth, canvas.offsetHeight)
+  renderer.setSize(canvas.offsetWidth, canvas.offsetHeight)
 
-console.log(canvas.offsetWidth)
-console.log(canvas.offsetHeight)
+  canvas.appendChild(renderer.domElement)
 
-canvas.appendChild(renderer.domElement)
+  const geometry = new THREE.BoxGeometry(2, 2, 2)
+  const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+  cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
 
-const geometry = new THREE.BoxGeometry(2, 2, 2)
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-camera.position.z = 5
+  camera.position.z = 5
+}
 
 function animate() {
   requestAnimationFrame(animate)
@@ -34,5 +33,11 @@ function animate() {
   cube.rotation.y += 0.01
   renderer.render(scene, camera)
 }
-
+function onWindowResize() {
+  camera.aspect = canvas.offsetWidth / canvas.offsetHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(canvas.offsetWidth, canvas.offsetHeight)
+}
+window.addEventListener('resize', onWindowResize, false)
+init()
 animate()
